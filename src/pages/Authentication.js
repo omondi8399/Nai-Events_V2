6476d94266ebfa179997aeccfea804b5e3a097d8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-09 16:43:29 +0300

    Updated code

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,4 +1,5 @@
-import { json, redirect} from 'react-router-dom'
+import { json, redirect } from 'react-router-dom';
+
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -7,37 +8,40 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-export async function action ({request}) {
-  const searchParams = new URL(request.url).searchParams
-  const mode = searchParams.get('mode') || 'login'
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
 
   if (mode !== 'login' && mode !== 'signup') {
-    throw json({message: 'Unsupported mode'}, {status: 422})
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
   }
 
-    const data = await request.formData()
-    const authData = {
-      email: data.get('email'),
-      password: data.get('password'),
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
 
-    }
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
 
-    const response = await fetch('http://localhost:8080/' + mode, {
-      method: 'POST',
-      header: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(authData)
-    })
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
 
-    if (response.status === 422 || response.status === 401) {
-      return response
-    }
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
 
-    if (!response.ok) {
-      throw json({message: 'Could not authenticate user.'}, { status: 500 })
-    }
+  const resData = await response.json();
+  const token = resData.token;
 
-    //soon: manage that token
-    return redirect('/')
+  localStorage.setItem('token', token);
+
+  return redirect('/');
 }
